eb89a78f56efa8d54a32538277710bd2f8e19400
Ashiqur Tusher <ashikurt77@gmail.com>
2019-11-22 11:02:33 +0600

    Updates

---
+++ precache-manifest.3daf40fb6d29e1440e6b27d12e0c35c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f86b511dc4dbf534754b24d63ca976e2",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "40b1f013113339faf9f5",
+    "url": "/ToDoList/static/css/main.b12f411a.chunk.css"
+  },
+  {
+    "revision": "b9eebd659a10869eac58",
+    "url": "/ToDoList/static/js/2.999e884c.chunk.js"
+  },
+  {
+    "revision": "40b1f013113339faf9f5",
+    "url": "/ToDoList/static/js/main.18aaca98.chunk.js"
+  },
+  {
+    "revision": "7c6a57889124396acba9",
+    "url": "/ToDoList/static/js/runtime-main.27033f35.js"
+  }
+]);
